3833dcfce1105474f7882b70ccbd40150cf5d283
Yujiro Kato <yu-katou@shibuya.co.jp>
2022-05-30 13:43:48 +0900

    コメントを追加。

--- YKToolkit.Blazor/MVVM/NotificationObject.cs
+++ YKToolkit.Blazor/MVVM/NotificationObject.cs
@@ -1,12 +1,30 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
+/// <summary>
+/// System.ComponentModel.INotifyPropertyChanged インターフェースを実装したプロパティ変更通知機能を提供します。
+/// </summary>
 public class NotificationObject : INotifyPropertyChanged
 {
+    /// <summary>
+    /// プロパティが変更されたときに発生します。
+    /// </summary>
     public event PropertyChangedEventHandler? PropertyChanged;
 
+    /// <summary>
+    /// PropertyChanged イベントを発行します。
+    /// </summary>
+    /// <param name="propertyName">プロパティ値が変更されたプロパティ名を指定します。</param>
     protected void RaisePropertyChanged([CallerMemberName]string? propertyName = null) => this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
+    /// <summary>
+    /// プロパティ値変更用ヘルパです。
+    /// </summary>
+    /// <param name="target">対象とするプロパティの実体を指定します。</param>
+    /// <param name="value">変更後の値を指定します。</param>
+    /// <param name="propertyName">プロパティ名を指定します。</param>
+    /// <typeparam name="T">プロパティの型を表します。</typeparam>
+    /// <returns>プロパティ値に変更があった場合に true を返します。</returns>
     protected bool SetProperty<T>(ref T target, T value, [CallerMemberName]string? propertyName = null)
     {
         if (EqualityComparer<T>.Default.Equals(target, value))
--- YKToolkit.Blazor/MVVM/ViewComponentBase.cs
+++ YKToolkit.Blazor/MVVM/ViewComponentBase.cs
@@ -1,6 +1,10 @@
 using System.ComponentModel;
 using Microsoft.AspNetCore.Components;
 
+/// <summary>
+/// Razor コンポーネントを MVVM インフラで使用するための機能を提供します。
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public abstract class ViewComponentBase<T> : ComponentBase
     where T : class
 {
@@ -17,7 +21,7 @@ public abstract class ViewComponentBase<T> : ComponentBase
     /// <summary>
     /// データコンテキストを取得または設定します。
     /// </summary>
-    /// <remark>DI コンテナからインジェクションされ、何らかのオブジェクトが必ず格納されることを想定しています。</remark>
+    /// <remark>DI コンテナからインジェクションされ、何らかのオブジェクトが必ず格納されることを想定しているため、null 非許容としています。</remark>
     [Parameter]
     [Inject]
     public T DataContext
