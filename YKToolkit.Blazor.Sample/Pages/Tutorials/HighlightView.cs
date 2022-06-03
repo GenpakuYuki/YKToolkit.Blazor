9478574e81f7bcb66b2f3c4e31cf1ed09a698acf
YKSoftware <wakuwakuwaffurunrun@outlook.jp>
2022-06-04 03:45:53 +0900

    ソースコードのハイライト機能を追加。

---
+++ YKToolkit.Blazor.Sample/Pages/Tutorials/HighlightView.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace YKToolkit.Blazor.Sample.Pages;
+
+public class HighlightView<T> : ViewComponentBase<T>
+    where T : class
+{
+    private IJSRuntime? _jsRuntime;
+    [Inject]
+    public IJSRuntime JsRuntime
+    {
+        get => this._jsRuntime ?? throw new ArgumentNullException(nameof(this._jsRuntime));
+        set => this._jsRuntime = value;
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await JsRuntime.InvokeVoidAsync("highlightSnippet");
+    }
+}
