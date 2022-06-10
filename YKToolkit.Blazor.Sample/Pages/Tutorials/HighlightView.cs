3d6317b7efe3686fc31e0846e9b8769b4a17ddc1
YKSoftware <wakuwakuwaffurunrun@outlook.jp>
2022-06-10 21:40:28 +0900

    DataContext が不要な場合のための HighlightView クラスを追加。

--- YKToolkit.Blazor.Sample/Pages/Tutorials/HighlightView.cs
+++ YKToolkit.Blazor.Sample/Pages/Tutorials/HighlightView.cs
@@ -3,6 +3,22 @@ using Microsoft.JSInterop;
 
 namespace YKToolkit.Blazor.Sample.Pages;
 
+public class HighlightView : ComponentBase
+{
+    private IJSRuntime? _jsRuntime;
+    [Inject]
+    public IJSRuntime JsRuntime
+    {
+        get => this._jsRuntime ?? throw new ArgumentNullException(nameof(this._jsRuntime));
+        set => this._jsRuntime = value;
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await JsRuntime.InvokeVoidAsync("highlightSnippet");
+    }
+}
+
 public class HighlightView<T> : ViewComponentBase<T>
     where T : class
 {
