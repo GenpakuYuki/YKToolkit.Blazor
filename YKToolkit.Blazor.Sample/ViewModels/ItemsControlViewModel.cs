8e0431f16f1ea057926e966f184d2502f19513f5
YKSoftware <wakuwakuwaffurunrun@outlook.jp>
2022-07-26 23:05:58 +0900

    ItemsControl コンポーネントのチュートリアルを追加。

---
+++ YKToolkit.Blazor.Sample/ViewModels/ItemsControlViewModel.cs
@@ -0,0 +1,19 @@
+﻿using YKToolkit.Blazor.Sample.Models;
+
+namespace YKToolkit.Blazor.Sample.ViewModels;
+
+/// <summary>
+/// Index コンポーネントに対する ViewModel を表します。
+/// </summary>
+public class ItemsControlViewModel : NotificationObject
+{
+    /// <summary>
+    /// 数値コレクションを取得します。
+    /// </summary>
+    public IEnumerable<int> Numbers { get; } = Enumerable.Range(0, 5).ToArray();
+
+    /// <summary>
+    /// 人物情報コレクションを取得します。
+    /// </summary>
+    public IEnumerable<Person> Persons { get; } = Enumerable.Range(0, 5).Select(x => new Person() { Name = $"太郎 {x}", Age = x }).ToArray();
+}
